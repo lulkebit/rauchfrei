8a978bba989beb3c8eb67fa8367118ac83c526fd
Luke <lukesnapacc@gmail.com>
2024-03-06 15:37:27 +0100

    Added Settings page and made use of localStorage

---
+++ src/scripts/settings.js
@@ -0,0 +1,16 @@
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+    // Den entsprechenden Wert aus dem localStorage abrufen
+    const storedValue = localStorage.getItem(input.name);
+    
+    // Wenn ein Wert im localStorage gefunden wurde, füge ihn in das Input-Feld ein
+    if (storedValue !== null) {
+        input.value = storedValue;
+    }
+
+    // Füge jedem InputFiled einen Listener hinzu, welcher das localStorage updated
+    input.addEventListener('change', () => {
+        localStorage.setItem(input.name, input.value);
+    });
+});
